4fbb44c3fcb4cec62aed801a035cebeaeb4fad08
aveein <avinamatya29@gmail.com>
2025-12-14 11:43:22 +0545

    Added search

--- controllers/BlackListController.js
+++ controllers/BlackListController.js
@@ -1,16 +1,33 @@
 
+const { Op } = require("sequelize");
 const BlackListUser = require("../models/BlackListUser");
 
 class BlackListController {
    
     static async getBlackList(req, res) {
         try {
-        
-            const blackList = await BlackListUser.findAll({order: [['createdAt', 'DESC']]});
+
+            const search = req.query.search;
+
+            const query = {
+            order: [['createdAt', 'DESC']],
+            };
+
+            // Add filter only if search param exists
+            if (search) {
+            query.where = {
+                [Op.or]: [
+                { full_name: { [Op.like]: `%${search}%` } },
+                { card_no: { [Op.like]: `%${search}%` } },
+                ],
+            };
+            }
+            const blackList = await BlackListUser.findAll(query);
             if(blackList){
                 return res.status(200).json({ data:blackList  });
             }
         } catch (error) {
+            console.log(error)
             return res.status(500).json({ message: 'Internal Server Error' });
         }
     }
